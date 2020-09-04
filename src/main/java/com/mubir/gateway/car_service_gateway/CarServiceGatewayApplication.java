03b085e5a1ae4738cd9eb525d61eb1d8d89ea898
Munir <info.mubir@gmail.com>
2020-09-04 12:30:33 +0900

    inital

---
+++ src/main/java/com/mubir/gateway/car_service_gateway/CarServiceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.mubir.gateway.car_service_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarServiceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarServiceGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mubir/gateway/car_service_gateway/CarServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mubir.gateway.car_service_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
